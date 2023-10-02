7b54133321925a6abed567a5b3ab35d047db5f4c
Paulo Rogerio <paulo@intranet>
2023-10-02 10:34:50 -0300

    Criando metodo executar no caso de uso registrar usuario

--- src/core/usuario/service/RegistrarUsuario.ts
+++ src/core/usuario/service/RegistrarUsuario.ts
@@ -1,5 +1,6 @@
 import { type } from "os";
 import CasoDeUso from "../../shared/CasoDeUso";
+import RepositorioUsuario from "./RepositorioUsuario";
 
 type Entrada = {
     nome: string
@@ -9,9 +10,17 @@ type Entrada = {
 
 export default class RegistrarUsuario implements CasoDeUso<Entrada, void>{
 
-    executar(dados: Entrada): Promise<void> {
-        const { nome, email, senha } = dados
+    constructor(
+        private readonly repositorio: RepositorioUsuario
+    ) { }
 
+    async executar(dados: Entrada): Promise<void> {
+        const { nome, email, senha } = dados
+        const UsuarioExistente = await this.repositorio.consultarPorEmail(email)
+        if (UsuarioExistente) {
+            throw new Error('Usuário já existe')
+        }
+        await this.repositorio.criar({ nome, email, senha })
     }
 
 }
