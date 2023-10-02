f7f754197f34450b89faaeb7e1e3cfb901d6253c
Paulo Rogerio <paulo@intranet>
2023-10-02 10:53:03 -0300

    Registrar rotas

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import { Elysia } from "elysia";
+import RepositorioUsuarioMemoria from "./external/memoria/RepositorioUsuarioMemoria";
+import RegistrarUsuario from "./core/usuario/service/RegistrarUsuario";
+import RegistrarUsuarioController from "./adapters/RegistrarUsuarioController";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+
+const repositorioUsuario = new RepositorioUsuarioMemoria();
+// Registrar rotas 
+
+const registrarUsuario = new RegistrarUsuario(repositorioUsuario);
+
+new RegistrarUsuarioController(app, registrarUsuario)
+
+
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
