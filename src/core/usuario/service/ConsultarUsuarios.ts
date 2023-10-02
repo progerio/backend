c8896145ecf677dd135c6865afed4077610f18ce
Paulo Rogerio <paulo@intranet>
2023-10-02 12:58:31 -0300

    Ajuste no caso de uso consultar usuarios

--- src/core/usuario/service/ConsultarUsuarios.ts
+++ src/core/usuario/service/ConsultarUsuarios.ts
@@ -6,8 +6,8 @@ export default class ConsultarUsuarios implements CasoDeUso<void, Usuario[]>{
 
     constructor(readonly repositorio: RepositorioUsuario) { }
 
-    async executar(): Promise<Usuario[]> {
-        return await this.repositorio.consultarTodos();
+    executar(): Promise<Usuario[]> {
+        return this.repositorio.consultarTodos();
     }
 
 }
